80c3118d9f8674055920220fbd702a7efad79603
Ivan Daum <ivan.daum@gmail.com>
2016-12-27 16:11:47 +0100

    feat(documentaion): add documentation

--- example/assets/js/main.js
+++ example/assets/js/main.js
@@ -1,4 +1,16 @@
-var Grabnslide = new Grabnslide({
-  container: document.querySelector('#slider1 ul'),
-  cell: document.querySelectorAll('#slider1 ul li')
+new Grabnslide({
+  container: document.querySelector('#slider1 ul')
+})
+
+new Grabnslide({
+  container: document.querySelector('#slider3 ul'),
+  cell: document.querySelectorAll('#slider3 ul li'),
+  triggerElement: document.querySelector('#slider-3-trigger')
+})
+
+$(document).ready(function() {
+  $(".nav-tabs a").on("click", function() {
+    $(this).closest('.nav-tabs').find('a').removeClass('active')
+    $(this).addClass('active')
+  })
 })
